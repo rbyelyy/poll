5eab74de95b18ae9e24f41a83d4f4984a043b1b5
Roman Byelyy <rbyelyy@gmail.com>
2015-06-25 18:08:46 +0200

    Added jquery for 'back' and 'next' buttons (reload page function)

--- app/static/javascript/increment_cookies.js
+++ app/static/javascript/increment_cookies.js
@@ -1,12 +1,30 @@
-$(function(){
-    $("#next").on('click',function () {
-        var question_id = parseInt(Cookies.get('question_id')) + 1
-        Cookies.set('question_id', question_id, { expires: 1, path: '' });
+$(function () {
+  $('#next').on('click', function () {
+    var question_id = parseInt(Cookies.get('question_id')) + 1
+    Cookies.set('question_id', question_id, {
+      expires: 1,
+      path: ''
     });
-        	$.ajax({
-            type: "GET",
-            url: "/sampletour",
-        })
+    $.ajax({
+      url: '',
+      context: document.body,
+      success: function (s, x) {
+        $(this).html(s);
+      }
+    });
+  });
+  $('#back').on('click', function () {
+    var question_id = parseInt(Cookies.get('question_id')) - 1
+    Cookies.set('question_id', question_id, {
+      expires: 1,
+      path: ''
+    });
+    $.ajax({
+      url: '',
+      context: document.body,
+      success: function (s, x) {
+        $(this).html(s);
+      }
+    });
+  });
 });
-
-
